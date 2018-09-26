4730edcca5371d75c2ad3489784bb46d4e06431c
Luke DeWitt <luke.dewitt@redspace.com>
2018-09-26 15:04:42 -0300

    fixed "class", added social sdks

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -7,7 +7,7 @@ const Home = props => {
       <h1 className="thatsweb__title">That's Web</h1>
       <h2 className="thatsweb__subtitle">A Meme Generator by Luke Dewitt</h2>
 
-      <div class="thatsweb-nav">
+      <div className="thatsweb-nav">
         <Link to="/tool">
           <button className="thatsweb-button -home">
             Be the Meme! &raquo;
